3d554b951b5e5219153da3dd9067d95b4dd1c39b
kokoa322 <tjsgus223@gmailcom>
2023-10-30 11:51:49 +0900

    first commit

---
+++ src/test/java/com/org/makgol/MakgolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.makgol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakgolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
