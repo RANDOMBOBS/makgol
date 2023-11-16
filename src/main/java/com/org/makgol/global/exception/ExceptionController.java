41609699fe7fa412625a88c943ed5e4ef3e24c13
kokoa322 <tjsgus223@gmail.com>
2023-11-16 17:23:41 +0900

    error page check

--- src/main/java/com/org/makgol/global/exception/ExceptionController.java
+++ src/main/java/com/org/makgol/global/exception/ExceptionController.java
@@ -10,11 +10,11 @@ import org.springframework.web.servlet.ModelAndView;
 public class ExceptionController {
 	//custom예외처리
 	@ExceptionHandler(value = CustomException.class)
-	public ModelAndView noDelivery(CustomException e) {
+	public ModelAndView Error(CustomException e) {
 		log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
 		ModelAndView mView=new ModelAndView();
 		mView.addObject("exception",e);
-		mView.setViewName("error/data_access");
+		mView.setViewName("jsp/error/error404");
 
 		return mView;
 	}
