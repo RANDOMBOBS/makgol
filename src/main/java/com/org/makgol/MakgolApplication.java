3a2f64a0761c508c498ff85bd4b6306a22a1c990
kimhanwool <gksdnf24@naver.com>
2023-11-16 19:47:00 +0900

    home

---
+++ src/main/java/com/org/makgol/MakgolApplication.java
@@ -0,0 +1,13 @@
+package com.org.makgol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakgolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakgolApplication.class, args);
+	}
+
+}
