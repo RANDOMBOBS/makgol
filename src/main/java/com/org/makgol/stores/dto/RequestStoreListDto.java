cf8a1d454824d427bdbdb973f72ffc922be68a09
Idooru <shere1765@gmail.com>
2023-11-21 18:14:06 +0900

    상품 리스트 요청 dto

---
+++ src/main/java/com/org/makgol/stores/dto/RequestStoreListDto.java
@@ -0,0 +1,15 @@
+package com.org.makgol.stores.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
+@Builder
+public class RequestStoreListDto {
+    private String longitude;
+    private String latitude;
+    private String keyword;
+    private String page;
+}
