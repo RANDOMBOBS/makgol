0e9afdf2985a256b69702acdda5db82135ee5fbf
kokoa322 <tjsgus223@gmailcom>
2023-11-30 11:40:28 +0900

    CompletableFuture 코드 정리

--- src/main/java/com/org/makgol/util/CompletableFuture.java
+++ src/main/java/com/org/makgol/util/CompletableFuture.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
-@Component
 public class CompletableFuture {
     static StoreService storeService;
 
