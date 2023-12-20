f15112e86a0cd3311e8e461557f0f90b25531c0e
kokoa322 <tjsgus223@gmail.com>
2023-12-20 17:16:56 +0900

    test

--- src/main/java/com/org/makgol/main/controller/MainController.java
+++ src/main/java/com/org/makgol/main/controller/MainController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -74,9 +76,9 @@ public class MainController {
 	}
 
 	@GetMapping("/health")
-	public String checkHealth() {
+	public HttpStatus checkHealth() {
 		log.info("public String checkHealth() {");
-		return "healthy";
+		return HttpStatus.OK;
 	}
 }
 
