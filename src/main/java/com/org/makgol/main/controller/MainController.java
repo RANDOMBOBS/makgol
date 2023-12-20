7e41bf18498bc31ab393c37303e85928a80d8d15
kokoa322 <tjsgus223@gmail.com>
2023-12-20 17:23:23 +0900

    code

--- src/main/java/com/org/makgol/main/controller/MainController.java
+++ src/main/java/com/org/makgol/main/controller/MainController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
