9f8711c3811370d6e2c71dc52745a2033ee94a6d
kokoa322 <tjsgus223@gmail.com>
2024-02-09 14:14:59 +0900

    redis 캐시 데이터 유지 시간 설정

--- src/main/java/com/org/makgol/util/redis/CacheService.java
+++ src/main/java/com/org/makgol/util/redis/CacheService.java
@@ -4,14 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class CacheService {
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
-    public void cacheQueryResult(String cacheKey, Object result) {
-        redisTemplate.opsForValue().set(cacheKey, result);
+    public void cacheQueryResult(String cacheKey, Object result, long expireTimeInMinutes) {
+        redisTemplate.opsForValue().set(cacheKey, result, expireTimeInMinutes, TimeUnit.MINUTES);
     }
 
     public Object getCachedQueryResult(String cacheKey) {
