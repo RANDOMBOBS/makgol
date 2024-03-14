54798bf1b2c32cff35ee4dd4d0c4b55451c3e36e
kokoa322 <tjsgus223@gmail.com>
2024-03-14 12:08:02 +0900

    캐시 체크

--- src/main/java/com/org/makgol/common/jwt/vo/TokenResponseVo.java
+++ src/main/java/com/org/makgol/common/jwt/vo/TokenResponseVo.java
@@ -1,14 +1,18 @@
 package com.org.makgol.common.jwt.vo;
 
 import lombok.*;
+import org.apache.ibatis.annotations.CacheNamespace;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class TokenResponseVo {
+@CacheNamespace(flushInterval = 1000000, size = 1024)
+public class TokenResponseVo implements Serializable {
+    private static final long serialVersionUID = 1L;
     private int     id;
     private String  token;
     private String  email;
