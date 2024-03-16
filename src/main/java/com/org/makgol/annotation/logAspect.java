42bdfd92189bfffce3f9ad84cafcc455261f52a1
kokoa322 <tjsgus223@gmail.com>
2024-03-16 15:52:26 +0900

    test

--- src/main/java/com/org/makgol/annotation/logAspect.java
+++ src/main/java/com/org/makgol/annotation/logAspect.java
@@ -1,10 +1,12 @@
 package com.org.makgol.annotation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @Aspect
 public class logAspect {
@@ -12,26 +14,44 @@ public class logAspect {
     @Around("com.org.makgol.annotation.LogExecutionTime")
     public Object logExcutionTimeAnno(ProceedingJoinPoint joinPoint) throws Throwable{
 
-        long startTime = System.currentTimeMillis();
 
-        Object proceed = joinPoint.proceed();
 
-        long stopTime = System.currentTimeMillis();
-        System.out.println(stopTime - startTime);
+        try {
+            long startTime = System.currentTimeMillis();
 
-        return proceed;
+            Object proceed = joinPoint.proceed();
+
+            long stopTime = System.currentTimeMillis();
+            System.out.println(stopTime - startTime);
+            return proceed;
+
+        } finally {
+            log.info("execute error{}", 1);
+        }
+
+
+
+
     }
 
     @Around("execution(* com.org.makgol.controller.*.*(..))")
     public Object logExcutionTimeVent(ProceedingJoinPoint joinPoint) throws Throwable{
 
-        long startTime = System.currentTimeMillis();
 
-        Object proceed = joinPoint.proceed();
 
-        long stopTime = System.currentTimeMillis();
-        System.out.println(stopTime - startTime);
+        try {
+            long startTime = System.currentTimeMillis();
 
-        return proceed;
+            Object proceed = joinPoint.proceed();
+
+            long stopTime = System.currentTimeMillis();
+            System.out.println(stopTime - startTime);
+            return proceed;
+
+        } finally {
+            log.info("execute error{}", 1);
+        }
+
+
     }
 }
